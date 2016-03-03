1456fcf1257c4f59971ea6898822532b11fc6371
boruoshihao <boruoshihao@gmail.com>
2016-03-03 15:25:51 -0500

    add rm 1st lib_hao in clean.py

--- clean.py
+++ clean.py
@@ -5,4 +5,5 @@ import subprocess
 import glob
 
 subprocess.call('rm -rf Modules', shell=True)
-subprocess.call('rm -rf *lib_hao', shell=True)
+subprocess.call('rm -rf *lib_hao_0', shell=True)
+subprocess.call('rm -rf *lib_hao_1', shell=True)
