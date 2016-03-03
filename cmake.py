8714f51042932d9e361215fc53940a859e4d32a2
boruoshihao <boruoshihao@gmail.com>
2016-03-03 14:56:49 -0500

    Change cmake.py to include LIBHAO itself.

--- cmake.py
+++ cmake.py
@@ -5,17 +5,20 @@ import subprocess
 typ  = sys.argv[1]
 dirc = sys.argv[2]
 if(typ=="mpi"):
+  os.environ['LIBHAO'] = "~/lib_hao/mpi"
   com="cmake -DCMAKE_CXX_COMPILER=mpic++ \
              -DCOMPILER_EXTRA_FLAG:STRING='-Wall -O3 -std=c++11' \
              -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO' \
              -DMODULE_EXTRA_PATH:STRING='~/cmake/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH=~/lib_hao/mpi "+dirc
 elif(typ=="serial"):
+  os.environ['LIBHAO'] = "~/lib_hao/serial"
   com="cmake -DCOMPILER_EXTRA_FLAG:STRING='-Wall -O3 -std=c++11' \
              -DCOMPILER_EXTRA_DEF:STRING=' '\
              -DMODULE_EXTRA_PATH:STRING='~/cmake/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH=~/lib_hao/serial "+dirc
 elif(typ=="storm"):
+  os.environ['LIBHAO'] = "~/lib_hao/mpi"
   os.environ['SPRNG'] = "~/sprng2.0"
   com="cmake -DCMAKE_CXX_COMPILER=mpicxx \
              -DCOMPILER_EXTRA_FLAG:STRING='-Wall -O3 -std=c++11' \
@@ -23,6 +26,7 @@ elif(typ=="storm"):
              -DMODULE_EXTRA_PATH:STRING='~/cmake/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH=~/lib_hao/mpi "+dirc
 elif(typ=="hu"):
+  os.environ['LIBHAO'] = "~/lib_hao/mpi"
   os.environ['SPRNG'] = "~/sprng2.0"
   com="cmake -DCMAKE_CXX_COMPILER=mpicxx \
              -DCOMPILER_EXTRA_FLAG:STRING='-Wall -O3 -march=corei7 -m64 -std=c++11' \
@@ -30,6 +34,7 @@ elif(typ=="hu"):
              -DMODULE_EXTRA_PATH:STRING='~/cmake/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH=~/lib_hao/mpi "+dirc
 elif(typ=="hu+magma"):
+  os.environ['LIBHAO'] = "~/lib_hao/mpimagma"
   os.environ['SPRNG'] = "~/sprng2.0"
   com="cmake -DCMAKE_CXX_COMPILER=mpicxx \
              -DCOMPILER_EXTRA_FLAG:STRING='-Wall -O3 -march=corei7 -m64 -std=c++11' \
@@ -38,6 +43,7 @@ elif(typ=="hu+magma"):
              -DMODULE_EXTRA_PATH:STRING='~/cmake/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH=~/lib_hao/mpimagma "+dirc
 elif(typ=="titan"):
+  os.environ['LIBHAO'] = "~/lib_hao/mpi"
   os.environ['SPRNG'] = os.environ['SPRNG_DIR']
   os.environ['FFTW']  = "/opt/fftw/3.3.4.0/interlagos"
   os.environ['ACML']  = "/opt/acml/5.3.1/gfortran64"
@@ -47,6 +53,7 @@ elif(typ=="titan"):
              -DMODULE_EXTRA_PATH:STRING='~/cmake/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH=~/lib_hao/mpi "+dirc
 elif(typ=="titan+magma"):
+  os.environ['LIBHAO'] = "~/lib_hao/mpimagma"
   os.environ['SPRNG'] = os.environ['SPRNG_DIR']
   os.environ['FFTW']  = "/opt/fftw/3.3.4.0/interlagos"
   os.environ['ACML']  = "/opt/acml/5.3.1/gfortran64"
