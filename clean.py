30a97314d52c290165c85f5ae2959051653088e6
Hao Shi <boruoshihao@gmail.com>
2016-04-28 16:16:46 -0400

    Update easy scripts for install lib_hao.

--- clean.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-import os
-import subprocess
-import glob
-
-subprocess.call('rm -rf Modules', shell=True)
-subprocess.call('rm -rf *lib_hao_*', shell=True)
---
+++ libhao_install.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+import os
+import subprocess
+import glob
+
+typ               = sys.argv[1]
+
+install_dirc_name = '~/lib'
+
+print "\033[1m" "Install custom cmake modules to "+install_dirc_name+"/Modules." "\033[0m"
+subprocess.call('rm -rf '+install_dirc_name+'/Modules', shell=True)
+subprocess.call('cp -r Modules '+install_dirc_name, shell=True)
+
+print "\033[1m" "Install lib_hao level 0 to "+install_dirc_name+"/lib_hao/"+typ+"." "\033[0m"
+subprocess.call("python cmake_code_script/lib_install.py "+typ+" '*lib_hao_0' "+ install_dirc_name+"/lib_hao", shell=True )
+
+print "\033[1m" "Install lib_hao level 1 to "+install_dirc_name+"/lib_hao/"+typ+"." "\033[0m"
+subprocess.call("python cmake_code_script/lib_install.py "+typ+" '*lib_hao_1' "+ install_dirc_name+"/lib_hao", shell=True )
